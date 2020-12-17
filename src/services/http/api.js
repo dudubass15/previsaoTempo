64efa717827ce123e50cb25b066f3aa751a0ba2f
Cadu <carlos.contato00@gmail.com>
2020-12-17 16:01:13 -0300

    Criado telas iniciais

--- src/services/http/api.js
+++ src/services/http/api.js
@@ -6,7 +6,7 @@ export default {
     getInfo(cidade) {
         axios({
             method: 'get',
-            url: 'http://api.openweathermap.org/data/2.5/forecast?q='+cidade+'&appid=2631155b530f7548a76c356d0ba307f9'
+            url: 'http://api.openweathermap.org/data/2.5/forecast?q='+cidade+'&appid={CHAVEAPI}'
         })
         .then(r => {
             console.log(r)
